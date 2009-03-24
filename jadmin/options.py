5a940fa2bb1a436e3a9612ff324699223862a6f4
James Pic <jamespic@gmail.com>
2009-03-24 15:25:26 +0000

    Added proper support for relation ajax widgets from jforms

--- jadmin/options.py
+++ jadmin/options.py
@@ -1,8 +1,12 @@
 from django.forms import models as form_models
 from django.db import models
 from django.contrib import admin
-from django.shortcuts import render_to_response
 from django import template
+from django.conf.urls.defaults import patterns
+from django.utils.functional import update_wrapper
+
+from django.core.urlresolvers import reverse
+from django.contrib.admin.util import quote
 
 from django.conf import settings
 
@@ -22,7 +26,62 @@ class ModelAdmin(admin.ModelAdmin):
             'js/php.min.js',
             '/media/js/urlify.js',
         )
-    
+    def get_urls(self):        
+        from django.conf.urls.defaults import patterns, url
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+        
+        info = self.admin_site.name, self.model._meta.app_label, self.model._meta.module_name
+
+        urlpatterns = patterns('',
+            url(r'^autocomplete/$',
+                wrap(self.autocomplete_view),
+                name='%sadmin_%s_%s_autocomplete' % info),
+        ) + super(ModelAdmin, self).get_urls()
+        return urlpatterns
+
+    def autocomplete_view(self, request):
+        import operator
+        from django.http import HttpResponse, HttpResponseNotFound
+        from django.db.models.query import QuerySet
+        from django.utils.encoding import smart_str
+
+        query = request.GET.get('q', None)
+        search_fields = request.GET.get('search_fields', None)
+        # TODO: figure is app_name and model_name are worth getting
+        # from the url or hack jforms.widgets
+        app_label = request.GET.get('app_label', None)
+        model_name = request.GET.get('model_name', None)
+
+        if search_fields and app_label and model_name and query:
+            def construct_search(field_name):
+                # use different lookup methods depending on the notation
+                if field_name.startswith('^'):
+                    return "%s__istartswith" % field_name[1:]
+                elif field_name.startswith('='):
+                    return "%s__iexact" % field_name[1:]
+                elif field_name.startswith('@'):
+                    return "%s__search" % field_name[1:]
+                else:
+                    return "%s__icontains" % field_name
+
+            model = models.get_model(app_label, model_name)
+            qs = model._default_manager.all()
+            for bit in query.split():
+                or_queries = [models.Q(**{construct_search(
+                    smart_str(field_name)): smart_str(bit)})
+                        for field_name in search_fields.split(',')]
+                other_qs = QuerySet(model)
+                other_qs.dup_select_related(qs)
+                other_qs = other_qs.filter(reduce(operator.or_, or_queries))
+                qs = qs & other_qs
+            data = ''.join([u'%s|%s\n' % (f.__unicode__(), f.pk) for f in qs])
+            return HttpResponse(data)
+        return HttpResponseNotFound()
+
     def changelist_view(self, request, extra_context=None):
         self.search_engine = self.create_search_engine(request)
         self.search_engine.parse_request(request)
@@ -66,8 +125,13 @@ class ModelAdmin(admin.ModelAdmin):
 
             jrelation_search_fields = self.get_jrelation_search_fields(db_field.rel.to)
 
+            info = self.admin_site.name, self.model._meta.app_label, self.model._meta.module_name
+            name = '%sadmin_%s_%s_autocomplete' % info
+
+            autocomplete_url = reverse(name)
+   
             if jrelation_search_fields:
-                kwargs['widget'] = widget(db_field.rel, jrelation_search_fields)
+                kwargs['widget'] = widget(db_field.rel, jrelation_search_fields, autocomplete_url=autocomplete_url)
         
         return super(ModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
