6c2ffb8a00ffef0c2017114b101239c3c62317e8
James Pic <jamespic@gmail.com>
2009-03-24 15:24:02 +0000

    Cleaned jadmin.AdminSite

--- jadmin/sites.py
+++ jadmin/sites.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 class AdminSite(admin.AdminSite):
-    #def index(self, request, extra_context=None):
-    #    return super(Admin2, self).index(request, {'foo': '*bar*'})
     pass
+
 admin.site = AdminSite()
